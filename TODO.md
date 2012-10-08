ea633b15f43aad79b54aa81230c248c910aaa8d4
Alejandro Morales <vamg008@gmail.com>
2012-10-08 00:04:08 -0600

    [dist] Moar awesomeness check out example

---
+++ TODO.md
@@ -0,0 +1,9 @@
+0.0.1 
+=====
+
+- Static Server inside the core of Ira 
+- Better error handling with domains
+- CLI tool
+- More clean templates
+- Fix API (for, do, on) is not really appropiated.I like it though
+- 
--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,13 @@
 
 /*test*/
 var fs = require('fs')
+var estatic = require('node-static')
 var Ira = require('../lib/ira')
 var ira = Ira.createApp({})
 
 // Error is an instance of ServerResponse, so res#* is imply on "this"
 
-ira.define('error', function ( code, error){
+ira.define('response:error', function ( code, error){
     if (code === 404 && !error) error = new Error('No encontrado')
     this.statusCode = code || 404 
     this.end(error.stack + '\n')
@@ -17,15 +18,36 @@ ira.define('env:production', function (conf){
 })
 
 
-ira.for('/').do(function (req, res){
-    var st = fs.createReadStream(__dirname + '/index.html')
-    st.on('error', function (){
-        res.end('ERROR')
-    })
-    st.pipe(res)
+var file = new estatic.Server(__dirname + '/../templates', {serverInfo: ira.get('pkg:name')});
+
+
+ira.define('server:static', function (req, res, next){
+    file.serve(req, res, next)
 })
 
+ira.define('response:render', function (file, vars){
+    fs.readFile(__dirname + '/../templates/' + file, 'utf8', function (err, fil){
+        fil = fil.replace(/<%=([\s\S]+?)%>/g, function(st, match, ind, left){
+            match = match.trim()
+            if (vars[match]) return vars[match]
+            else return void 0
+        })
+        this.setHeader('Content-Type', 'text/html')
+        this.end(fil)
+    }.bind(this))
+})
 
+ira.for('/').do(function (req, res){
+    res.render('index.html.tmpl',{
+        title: 'Ira: Web Framework',
+        name: 'Ira',
+        description: ira.get('pkg:description')
+    })
+})
+
+ira.for('/hola').do(function (req, res){
+    res.end('HOOOOLAAA')
+})
 ira.listen(8100, function(){
     console.log('[*] IRA: Server listening on', this.address().port)
 })
